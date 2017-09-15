9abacfe84038a92d6d0f0baf9c87a113fa9b4738
alexqdjay <alexqdjay@126.com>
2017-09-15 22:27:16 +0800

    init

---
+++ examples/gateway/src/main/java/me/ucake/sc/gateway/web/EchoController.java
@@ -0,0 +1,48 @@
+/**
+ * (C) Copyright 2017 alex qian
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package me.ucake.sc.gateway.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * EchoController
+ *
+ * @author alex.Q
+ * @date 2017/8/6
+ */
+@RequestMapping("/echo")
+@RestController
+public class EchoController {
+
+    private ServerProperties serverProperties;
+
+    @Autowired
+    public void setServerProperties(ServerProperties serverProperties) {
+        this.serverProperties = serverProperties;
+    }
+
+    @RequestMapping("/{msg}")
+    public String echo(@PathVariable String msg) {
+        return "zuul:" + msg;
+    }
+
+}
---
+++ examples/gateway/src/main/java/me/ucake/sc/gateway/web/ServerProperties.java
@@ -0,0 +1,52 @@
+/**
+ * (C) Copyright 2017 alex qian
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package me.ucake.sc.gateway.web;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * ServerProperties
+ *
+ * @author alex.Q
+ * @date 2017/8/11
+ */
+@Component
+@EnableConfigurationProperties(value={ServerProperties.class})
+@ConfigurationProperties(prefix = "server")
+public class ServerProperties {
+    private String port;
+    private String name;
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
